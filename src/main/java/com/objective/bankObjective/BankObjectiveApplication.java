9d5b106b41000811d7c367571e65998af312755e
Lucas de Lima Chaves <15lucas.lima15@gmail.com>
2025-02-08 11:56:05 -0500

    feat: adicionando swagger para documentação

--- src/main/java/com/objective/bankObjective/BankObjectiveApplication.java
+++ src/main/java/com/objective/bankObjective/BankObjectiveApplication.java
@@ -1,4 +1,4 @@
-package com.objective.bankObjective.application;
+package com.objective.bankObjective;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
