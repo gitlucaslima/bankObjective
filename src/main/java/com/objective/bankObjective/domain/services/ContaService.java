8b048ce55d69c85eb04dea8b1aba85528a7de9a4
Lucas de Lima Chaves <15lucas.lima15@gmail.com>
2025-02-08 11:53:59 -0500

    feat: criação dos services e implementaçãod as regas de negocio

--- src/main/java/com/objective/bankObjective/domain/services/ContaService.java
+++ src/main/java/com/objective/bankObjective/domain/services/ContaService.java
@@ -1,4 +1,47 @@
 package com.objective.bankObjective.domain.services;
 
+import com.objective.bankObjective.domain.models.Conta;
+import com.objective.bankObjective.domain.models.dtos.ContaRequestDto;
+import com.objective.bankObjective.domain.models.dtos.ContaResponseDto;
+import com.objective.bankObjective.infrastructure.repository.ContaRepository;
+import com.objective.bankObjective.infrastructure.exceptions.AccountExistingException;
+import com.objective.bankObjective.infrastructure.exceptions.AccountNotExistingException;
+import com.objective.bankObjective.infrastructure.exceptions.NegativeBalanceException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
 public class ContaService {
+
+    private final ContaRepository contaRepository;
+
+    @Transactional
+    public ContaResponseDto criarConta(ContaRequestDto contaRequestDto) throws AccountExistingException, NegativeBalanceException {
+
+        if (contaRepository.findByNumero(contaRequestDto.numeroConta()).isPresent())
+            throw new AccountExistingException();
+
+        if (contaRequestDto.saldo().compareTo(BigDecimal.ZERO) < 0)
+            throw new NegativeBalanceException();
+
+        Conta conta = Conta.builder()
+                .saldo(contaRequestDto.saldo())
+                .numero(contaRequestDto.numeroConta())
+                .build();
+
+        Conta contaSalva = contaRepository.save(conta);
+
+        return new ContaResponseDto(contaSalva.getNumero(), contaSalva.getSaldo());
+    }
+
+    public ContaResponseDto find(Long numeroConta) throws AccountNotExistingException {
+        var conta = contaRepository.findByNumero(numeroConta).orElseThrow(AccountNotExistingException::new);
+        return new ContaResponseDto(conta.getNumero(), conta.getSaldo());
+    }
+
+
 }
--- src/main/java/com/objective/bankObjective/domain/services/TransacaoService.java
+++ src/main/java/com/objective/bankObjective/domain/services/TransacaoService.java
@@ -1,4 +1,58 @@
 package com.objective.bankObjective.domain.services;
 
+import com.objective.bankObjective.domain.models.Conta;
+import com.objective.bankObjective.domain.models.FormaPagamento;
+import com.objective.bankObjective.domain.models.dtos.TransacaoRequestDto;
+import com.objective.bankObjective.domain.models.dtos.TransacaoResponseDto;
+import com.objective.bankObjective.infrastructure.repository.ContaRepository;
+import com.objective.bankObjective.domain.strategy.FormaPagamentoStrategy;
+import com.objective.bankObjective.infrastructure.exceptions.AccountNotExistingException;
+import com.objective.bankObjective.infrastructure.exceptions.InsufficientBalanceException;
+import com.objective.bankObjective.infrastructure.exceptions.NotFoundPaymentTypeException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
 public class TransacaoService {
+
+    private final ContaRepository contaRepository;
+    private final Map<FormaPagamento, FormaPagamentoStrategy> processaPagamentoPorTipo;
+
+    @Transactional
+    public TransacaoResponseDto novaTransacao(TransacaoRequestDto transacaoRequest) throws NotFoundPaymentTypeException, InsufficientBalanceException, AccountNotExistingException {
+
+        var contaEncontrada = contaRepository.findByNumero(transacaoRequest.numeroConta())
+                .orElseThrow(AccountNotExistingException::new);
+
+        FormaPagamento formaPagamento = FormaPagamento.fromCodigo(transacaoRequest.formaPagamento());
+
+        FormaPagamentoStrategy estrategiaDePagamento = processaPagamentoPorTipo.get(formaPagamento);
+
+        if (estrategiaDePagamento == null) {
+            throw new NotFoundPaymentTypeException();
+        }
+
+        if (transacaoRequest.valor().compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("O valor da transação deve ser maior que zero");
+        }
+
+        var valorFinal = estrategiaDePagamento.processarPagamento(transacaoRequest.valor());
+
+        if (contaEncontrada.getSaldo().compareTo(valorFinal) < 0) {
+            throw new InsufficientBalanceException();
+        }
+
+        contaEncontrada.setSaldo((contaEncontrada.getSaldo().subtract(valorFinal).setScale(2, RoundingMode.HALF_EVEN)));
+
+        Conta contaAtualizada = contaRepository.save(contaEncontrada);
+
+        return new TransacaoResponseDto(contaAtualizada.getNumero(), contaAtualizada.getSaldo());
+    }
+
 }
